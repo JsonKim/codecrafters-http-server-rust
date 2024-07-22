616ef3e128c529fb6fd38f753b7f4d5a8848a2a7
Jason Kim <actcrazy@gmail.com>
2024-07-22 22:42:43 +0900

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
 // Uncomment this block to pass the first stage
-use std::net::TcpListener;
+use std::{
+    io::Error,
+    io::Write,
+    net::{TcpListener, TcpStream},
+};
+
+fn handle_client(mut stream: TcpStream, message: &str) -> Result<(), Error> {
+    stream.write(message.as_bytes())?;
+    stream.flush()?;
+    Ok(())
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -13,6 +23,9 @@ fn main() {
         match stream {
             Ok(_stream) => {
                 println!("accepted new connection");
+                if let Err(e) = handle_client(_stream, "HTTP/1.1 200 OK\r\n\r\n") {
+                    eprintln!("Error handling client: {}", e);
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
