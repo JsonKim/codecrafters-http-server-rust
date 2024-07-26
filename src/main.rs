4a78d785bad5826d5d6fe7af7bbeb1008399c1e9
Jason Kim <actcrazy@gmail.com>
2024-07-26 21:10:29 +0900

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,39 @@
 // Uncomment this block to pass the first stage
 use std::{
-    io::{Error, Read, Write},
-    net::{TcpListener, TcpStream},
+    collections::HashMap, io::{Error, Read, Write}, net::{TcpListener, TcpStream}
 };
 
 enum RouteContent<'a> {
     Index,
     Echo(&'a str),
+    UserAgent,
     NotFound,
 }
 
-fn parse_route(input: &str) -> RouteContent {
+fn parse_route<'a>(input: &'a str) -> RouteContent {
     if input == "/" {
         RouteContent::Index
+    } else if input == "/user-agent" {
+        RouteContent::UserAgent
     } else if let Some(content) = input.strip_prefix("/echo/") {
         RouteContent::Echo(content)
     } else {
         RouteContent::NotFound
     }
+}
+
+fn parse_text_to_map(text: &str) -> HashMap<String, String> {
+    text.lines()
+        .skip(1)
+        .filter_map(|line| {
+            let parts: Vec<&str> = line.splitn(2, ": ").collect();
+            if parts.len() == 2 {
+                Some((parts[0].trim().to_string(), parts[1].trim().to_string()))
+            } else {
+                None
+            }
+        })
+        .collect()
 }
 
 fn get_path(request: &str) -> &str {
@@ -48,12 +64,17 @@ fn main() {
                 let bytes_read = stream.read(&mut buffer).unwrap();
                 let request = String::from_utf8_lossy(&buffer[..bytes_read]);
                 let path = get_path(&request);
+                let headers = parse_text_to_map(&request);
 
                 let message = match parse_route(path) {
                     RouteContent::Index => "HTTP/1.1 200 OK\r\n\r\n".to_string(),
                     RouteContent::Echo(content) => 
                         format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", content.len(), content),
                     RouteContent::NotFound => "HTTP/1.1 404 Not Found\r\n\r\n".to_string(),
+                    RouteContent::UserAgent => match headers.get("User-Agent") {
+                        Some(user_agent) => format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", user_agent.len(), user_agent),
+                        None => "HTTP/1.1 404 Not Found\r\n\r\n".to_string(),
+                    }
                 };
 
                 if let Err(e) = handle_client(stream, message.as_str()) {
